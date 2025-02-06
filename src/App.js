8923f5840a71831fbfb7431e042db7d16323b93b
Jukkis Ahonen <jukkisahonen@gmail.com>
2025-02-06 19:12:04 +0200

    Added Firebase library and sign-in route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBE9KkVgblyQT_YTfePeuMBTYSLSFAffE",
+  authDomain: "nxtstride-application.firebaseapp.com",
+  projectId: "nxtstride-application",
+  storageBucket: "nxtstride-application.firebasestorage.app",
+  messagingSenderId: "365588873250",
+  appId: "1:365588873250:web:b8a93680309f2a37680a7b",
+  measurementId: "G-HWN06EYJH1"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App = () => {
 
@@ -9,6 +26,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}/>
+        <Route path='sign-In' element={<SignIn />}/>
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,16 +3,25 @@ import { Outlet, Link } from 'react-router-dom';
 
 import './navigation.styles.scss';
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'; 
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
       <Fragment>
         <div className= 'navbar'>
           <Link classname='logo-container' to='/'>
-            <div>Logo</div>
+            <CrwnLogo classname='logo'/>
           </Link>
           <div classname='nav-links-container'>
             <Link classname='nav-link' to='/shop'>
                 SHOP
+            </Link>
+          </div>
+          <div classname='nav-links-container'>
+            <Link classname='nav-link' to='/sign-in'>
+                SIGN-IN
             </Link>
           </div>
         </div>
