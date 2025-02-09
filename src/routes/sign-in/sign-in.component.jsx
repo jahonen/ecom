66189198f822ca4046bbbbf50ad23968d988e606
Jukkis Ahonen <jukkisahonen@gmail.com>
2025-02-09 17:37:28 +0200

    Added google redirect auth support

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,31 @@
 import { 
+    auth,
     signInWithGooglePopup,
+    signInWithGoogleRedirect,
     createUserDocumentFromAuth,
- } from "../../utils/firebase/firebase.utils"
+ } from "../../utils/firebase/firebase.utils";
+ import { useEffect } from 'react';
+ import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);
+
+    const logGooglePopup = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
-    
+    } 
+
     return (
         <div>
             <h1>This is the sign-in page.</h1> 
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={logGooglePopup}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 
